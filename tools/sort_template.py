218241d39f4c6e4527c2c8e76a7b70eb29907b87
Jaeseok Yoon <jake.yoon@kaist.ac.kr>
2023-08-15 01:01:24 +0900

    Implement sort

--- tools/sort_template.py
+++ tools/sort_template.py
@@ -123,6 +123,18 @@ def main():
             args.key = 'mpqa_dev_eval_acc'
             args.test_key = 'mpqa_test_eval_acc'
             print_name = condition['task_name']
+        elif condition['task_name'] == 'boolq':
+            args.key = 'boolq_dev_eval_acc'
+            args.test_key = 'boolq_test_eval_acc'
+            print_name = condition['task_name']
+        elif condition['task_name'] == 'cb':
+            args.key = 'cb_dev_eval_acc'
+            args.test_key = 'cb_test_eval_acc'
+            print_name = condition['task_name']
+        elif condition['task_name'] == 'multirc':
+            args.key = 'multirc_dev_eval_f1'
+            args.test_key = 'multirc_test_eval_f1'
+            print_name = condition['task_name']
         else:
             raise NotImplementedError
 
