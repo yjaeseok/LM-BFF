852515114de56d695331e9526a10487286b9a80b
Jaeseok Yoon <jake.yoon@kaist.ac.kr>
2023-08-14 22:01:46 +0900

    Update code

--- data/download_superglue.py
+++ data/download_superglue.py
@@ -1,8 +1,11 @@
+import os
 from datasets import load_dataset
 
 tasks = ['boolq', 'cb', 'multirc']
 
 for task in tasks:
+    if not os.path.exists(f"original/{task}"):
+        os.mkdir(f"original/{task}")
     dataset = load_dataset('super_glue', 'rte')
     dataset['train'].to_csv(f"original/{task}/train.tsv", sep='\t', index=False)
-    dataset['dev'].to_csv(f"original/{task}/dev.tsv", sep='\t', index=False)
+    dataset['validation'].to_csv(f"original/{task}/dev.tsv", sep='\t', index=False)
--- tools/generate_k_shot_data.py
+++ tools/generate_k_shot_data.py
@@ -38,7 +38,7 @@ def get_label(task, line):
 def load_datasets(data_dir, tasks):
     datasets = {}
     for task in tasks:
-        if task in ["MNLI", "MRPC", "QNLI", "QQP", "RTE", "SNLI", "SST-2", "STS-B", "WNLI", "CoLA"]:
+        if task in ["MNLI", "MRPC", "QNLI", "QQP", "RTE", "SNLI", "SST-2", "STS-B", "WNLI", "CoLA", 'boolq', 'cb', 'multirc']:
             # GLUE style (tsv)
             dataset = {}
             dirname = os.path.join(data_dir, task)
@@ -69,7 +69,7 @@ def split_header(task, lines):
     """
     if task in ["CoLA"]:
         return [], lines
-    elif task in ["MNLI", "MRPC", "QNLI", "QQP", "RTE", "SNLI", "SST-2", "STS-B", "WNLI"]:
+    elif task in ["MNLI", "MRPC", "QNLI", "QQP", "RTE", "SNLI", "SST-2", "STS-B", "WNLI", 'cb', 'multirc', 'boolq']:
         return lines[0:1], lines[1:]
     else:
         raise ValueError("Unknown GLUE task.")
@@ -79,7 +79,7 @@ def main():
     parser.add_argument("--k", type=int, default=16,
         help="Training examples for each class.")
     parser.add_argument("--task", type=str, nargs="+", 
-        default=['SST-2', 'sst-5', 'mr', 'cr', 'mpqa', 'subj', 'trec', 'CoLA', 'MRPC', 'QQP', 'STS-B', 'MNLI', 'SNLI', 'QNLI', 'RTE'],
+        default=['SST-2', 'sst-5', 'mr', 'cr', 'mpqa', 'subj', 'trec', 'CoLA', 'MRPC', 'QQP', 'STS-B', 'MNLI', 'SNLI', 'QNLI', 'RTE', 'cb', 'multirc', 'boolq'],
         help="Task names")
     parser.add_argument("--seed", type=int, nargs="+", 
         default=[100, 13, 21, 42, 87],
@@ -104,7 +104,7 @@ def main():
 
             # Shuffle the training set
             print("| Task = %s" % (task))
-            if task in ["MNLI", "MRPC", "QNLI", "QQP", "RTE", "SNLI", "SST-2", "STS-B", "WNLI", "CoLA"]:
+            if task in ["MNLI", "MRPC", "QNLI", "QQP", "RTE", "SNLI", "SST-2", "STS-B", "WNLI", "CoLA", 'boolq' ,'cb', 'multirc']:
                 # GLUE style 
                 train_header, train_lines = split_header(task, dataset["train"])
                 np.random.shuffle(train_lines)
@@ -119,7 +119,7 @@ def main():
             os.makedirs(setting_dir, exist_ok=True)
 
             # Write test splits
-            if task in ["MNLI", "MRPC", "QNLI", "QQP", "RTE", "SNLI", "SST-2", "STS-B", "WNLI", "CoLA"]:
+            if task in ["MNLI", "MRPC", "QNLI", "QQP", "RTE", "SNLI", "SST-2", "STS-B", "WNLI", "CoLA", 'boolq' ,'cb', 'multirc']:
                 # GLUE style
                 # Use the original development set as the test set (the original test sets are not publicly available)
                 for split, lines in dataset.items():
@@ -143,7 +143,7 @@ def main():
                 else:
                     label_list[label].append(line)
             
-            if task in ["MNLI", "MRPC", "QNLI", "QQP", "RTE", "SNLI", "SST-2", "STS-B", "WNLI", "CoLA"]:
+            if task in ["MNLI", "MRPC", "QNLI", "QQP", "RTE", "SNLI", "SST-2", "STS-B", "WNLI", "CoLA", 'boolq' ,'cb', 'multirc']:
                 with open(os.path.join(setting_dir, "train.tsv"), "w") as f:
                     for line in train_header:
                         f.write(line)
