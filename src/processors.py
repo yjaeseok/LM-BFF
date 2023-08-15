b97aef706ad95015053a59b9db9c555c2be86b01
Jaeseok Yoon <jake.yoon@kaist.ac.kr>
2023-08-15 22:14:41 +0900

    fixed bug

--- src/processors.py
+++ src/processors.py
@@ -757,7 +757,7 @@ compute_metrics_mapping = {
     "mpqa": text_classification_metrics,
     "boolq": text_classification_metrics,
     "cb": text_classification_metrics,
-    "multirc": f1_metric
+    "multirc": text_classification_metrics
 }
 
 # For regression task only: median
